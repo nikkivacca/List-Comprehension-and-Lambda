8ec4f17b7569f633e831c5db92a25c8199e72e86
Nichol Vacca <nichol_vacca1@baylor.edu>
2022-03-21 15:35:23 -0500

    class

--- list_comprehension_start_file.py
+++ list_comprehension_start_file.py
@@ -57,7 +57,8 @@ for i in original_list:
 
 numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
-
+newlist = [i for i in numbers if i >=0]
+print(newlist)
 
 
 ## 2 create a list of integers which specify the length of each word in
@@ -65,7 +66,8 @@ numbers = [34.6, -203.4, 44.9, 68.3, -12.2, 44.6, 12.7]
 
 sentence = "the quick brown fox jumps over the lazy dog"
 words = sentence.split()
-
+integer_list = [len(i) for i in words if i != "the"]
+print(integer_list)
 
 
 ## Given dictionary is consisted of vehicles and their weights in kilograms. 
@@ -75,24 +77,27 @@ words = sentence.split()
 dict={"Sedan": 1500, "SUV": 2000, "Pickup": 2500, "Minivan": 1600, "Van": 2400, 
 "Semi": 13600, "Bicycle": 7, "Motorcycle": 110}
 
-
+names = [i.upper() for i in dict if dict[i] <= 5000]
+print(names)
 
 
 ## Find all the numbers from 1 to 1000 that have a 4 in them
 
-
+numbers = [i for i in range (1,1000) if '4' in str(i) ]
+print(numbers)
 
 ## count how many times the word 'the' appears in the text file - 'sometext.txt'
+
+myfile = open('sometext.txt', 'r')
+lines = myfile.splitlines()
+words = lines.split()
+print(words)
 
 
 
 ## Extract the numbers from the following phrase ##
 
-phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each " +
-"event, with about 3 or 4 that were classifled as serious per event.'
-
-
-
-
-
-
+phrase = 'In 1984 there were 13 instances of a protest with over 1000 people attending. On average there were 15 reported injuries at each event, with about 3 or 4 that were classifled as serious per event.'
+words = phrase.split()
+numbers =[x for x in words if x.isnumeric()]
+print(numbers)
